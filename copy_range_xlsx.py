4fa04914eff2eb5617cf2ec63081c44903165207
Christian Van de Werken <cvdw@werken.com>
2019-03-23 20:21:07 -0400

    forgot the copy and paste functions

--- copy_range_xlsx.py
+++ copy_range_xlsx.py
@@ -10,6 +10,24 @@ col =
 
 ### 
 
+def copyRange(startCol, startRow, endCol, endRow, sheet):
+        rangeSelected = []
+        for i in range(startRow,endRow + 1,1):
+                rowSelected = []
+                for j in range(startCol,endCol+1,1):
+                        rowSelected.append(sheet.cell(row = i, column = j).value)
+                rangeSelected.append(rowSelected)
+        return rangeSelected
+
+def pasteRange(startCol, startRow, endCol, endRow, sheetReceiving, copiedData):
+        countRow = 0
+        for i in range(startRow,endRow+1,1):
+                countCol = 0
+                for j in range(startCol,endCol+1,1):
+                        sheetReceiving.cell(row = i, column = j).value = copiedData[countRow][countCol]
+                        countCol += 1
+                countRow += 1
+
 for xlsxToProcess in glob(processDirectory + "*.xlsx"): 
     fileInProcess = os.path.basename(xlsxToProcess)
     wb = openpyxl.load_workbook(xlsxToProcess)
